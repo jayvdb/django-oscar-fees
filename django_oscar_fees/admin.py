82aa400b2722b130aa8e3a3fcaba334c0fb42802
John Vandenberg <jayvdb@gmail.com>
2021-02-24 21:37:34 +0800

    admin: Add ConditionalFeeAdmin

--- django_oscar_fees/admin.py
+++ django_oscar_fees/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from .models import Condition, ConditionalFee, Fee, FeeLine, OrderFee
 
+class ConditionalFeeAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(Condition)
-admin.site.register(ConditionalFee)
+admin.site.register(ConditionalFee, ConditionalFeeAdmin)
 admin.site.register(Fee)
 admin.site.register(FeeLine)
 admin.site.register(OrderFee)
